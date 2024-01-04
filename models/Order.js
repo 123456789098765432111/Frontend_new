b910739a1f4186d2c96954ceab9e508d1d1ebdbb
Umidjon <127615615+123456789098765432111@users.noreply.github.com>
2024-01-04 22:28:01 +0500

    first commit

--- models/Order.js
+++ models/Order.js
@@ -2,12 +2,7 @@ import {model, models, Schema} from "mongoose";
 
 const OrderSchema = new Schema({
   line_items:Object,
-  name:String,
-  email:String,
-  city:String,
-  zip:String,
   address:String,
-  country:String,
   paid:Boolean,
 }, {
   timestamps: true,
