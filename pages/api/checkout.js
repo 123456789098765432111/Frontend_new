bcddf9cf9043209d807715a9f18fe5860caa0aa4
Umidjon <127615615+Umidjon-developer05@users.noreply.github.com>
2024-05-21 10:06:30 +0500

    status code

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -1,60 +1,49 @@
 import { mongooseConnect } from "@/lib/mongoose";
 import { Order } from "@/models/Order";
 import { Product } from "@/models/Product";
-// const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
-    res.json('Should be a post request');
+    res.status(405).json({ message: 'Method Not Allowed' });
     return;
   }
 
-
-  const {  address, phone , email,  cartProducts } = req.body;
+  const { address, phone, email, cartProducts } = req.body;
 
   await mongooseConnect();
 
   const productIds = cartProducts;
-  const uniqueIds = [... new Set(productIds)];
-  const productsInfo = await Product.find({ _id: uniqueIds });
+  const uniqueIds = [...new Set(productIds)];
+  const productsInfo = await Product.find({ _id: { $in: uniqueIds } });
 
   let line_items = [];
 
   for (const productId of uniqueIds) {
     const productInfo = productsInfo.find(p => p._id.toString() === productId);
 
-    const quantity = productIds.filter(id => id === productId)?.length || 0;
+    const quantity = productIds.filter(id => id === productId).length;
 
     if (quantity > 0 && productInfo) {
-      line_items.push(
-        {
-          quantity,
-          price_data: {
-            currency: 'KES',
-            product_data: { name: productInfo.title },
-            unit_amount: quantity * productInfo.price * 100 ,
-          },
-
-        }
-      )
+      line_items.push({
+        quantity,
+        price_data: {
+          currency: 'KES',
+          product_data: { name: productInfo.title },
+          unit_amount: productInfo.price * 100, // Ensure the price is correctly multiplied by 100
+        },
+      });
     }
   }
 
-   await Order.create({
-    line_items, address,   email , phone , paid: false
-  })
+  await Order.create({
+    line_items, 
+    address, 
+    email, 
+    phone, 
+    paid: false,
+  });
 
-  // const session = await stripe.checkout.sessions.create({
-  //   line_items,
-  //   mode: 'payment',
-  //   customer_email: email,
-  //   success_url: process.env.SUCCESS_URL + '/cart?success=1',
-  //   cancel_url: process.env.SUCCESS_URL + '/cart?canceled=1',
-  //   metadata: { orderId: orderDoc._id.toString(), test: 'ok' }
-  // })
-
-  res.json({
-    url: session.url,
-  })
-
+  res.status(200).json({
+    message: 'Order created successfully',
+  });
 }
